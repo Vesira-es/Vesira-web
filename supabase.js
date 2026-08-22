147a397dcfdb587e0ed4bbc8fee0805991aab571
Alejandro Jesús Guadalupe Travieso <alejandro@MacBook-Air-de-Alejandro.local>
2026-08-22 21:45:20 +0100

    carrusel colecciones hecho salvo un fallo en movimiento

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 console.log("SUPABASE.JS SE HA CARGADO");
 
-const SUPABASE_URL = "https://mpuisitgepxfhukfvleu.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wdWlzaXRnZXB4Zmh1a2Z2bGV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYyMjU0NjMsImV4cCI6MjEwMTgwMTQ2M30.U15nDVvMFos0lRn8xinXJVTzNKjpWFoDx3BPXd6sd_s";
+const SUPABASE_URL = "https://smxdiexgxxwjlvsdmzrq.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNteGRpZXhneHh3amx2c2RtenJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc0MjI4MjYsImV4cCI6MjEwMjk5ODgyNn0.Li9_BZoSoSa2XoKnNr4bBO5N2N8GL9VAHmX3ZxL2dWE";
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
